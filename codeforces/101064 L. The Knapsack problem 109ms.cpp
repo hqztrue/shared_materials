bbc3fcb67c48c25e0461ab9d2bf564829f5d3619
hqztrue <hqztrue@sina.com>
2020-07-26 11:36:39 -0500

    .

--- codeforces/101064 L. The Knapsack problem 109ms.cpp
+++ codeforces/101064 L. The Knapsack problem 109ms.cpp
@@ -9,7 +9,7 @@ int main(){
 	for (int i=0;i<n;++i)scanf("%d%d",w+i,c+i),u=max(u,w[i]),id[i]=i;
 	sort(id,id+n,cmp);
 	for (int i=1;i<=min(W,u*u);++i)
-		for (int j=0;j<min((int)ceil(2.*u*u/i),n);++j)
+		for (int j=0;j<min((int)ceil(3.*u*u/i),n);++j)
 			if (w[id[j]]<=i)f[i]=max(f[i],f[i-w[id[j]]]+c[id[j]]);
 	ll m=ceil(1.*max(0,W-u*u)/w[id[0]]);
 	cout<<f[W-m*w[id[0]]]+m*c[id[0]]<<endl;
