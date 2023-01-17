14e242f14c155460492f347a45ee3b62cde1f383
hqztrue <hqztrue@sina.com>
2023-01-17 01:04:45 -0600

    Update count_language.py

--- LeetCode/LC_contest_lang/count_language.py
+++ LeetCode/LC_contest_lang/count_language.py
@@ -1,22 +1,30 @@
 from collections import Counter,defaultdict
-f=open("weekly-contest-327_lang_P1.txt")
-rank=defaultdict(list)
-a=[]
-for s in f.readlines():
-    l,r=s.split(',')[0],int(s.split(',')[1][1:])
-    a.append(l)
-    rank[l].append(r)
-
-c=Counter(a)
-print('total=',sum(c.values()))
-print(c)
-#print(rank)
-
 import matplotlib.pyplot as plt
-lang=['cpp','java','python','golang']
-rank['python']+=rank['python3']
-#plt.hist([rank[s] for s in lang], 20, label=lang) #histtype='step'
-plt.hist([rank[s] for s in lang], 1000, histtype='step', cumulative=True, label=lang)
-plt.legend()
-plt.show()
+def draw(name):
+    f=open(name)
+    rank=defaultdict(list)
+    a=[]
+    for s in f.readlines():
+        l,r=s.split(',')[0],int(s.split(',')[1][1:])
+        a.append(l)
+        rank[l].append(r)
 
+    c=Counter(a)
+    print('total=',sum(c.values()))
+    print(c)
+    #print(rank)
+
+    lang=['cpp','java','python','golang','javascript']
+    rank['python']+=rank['python3']
+    plt.figure()
+    plt.title(name)
+    #plt.hist([rank[s] for s in lang], 20, label=lang) #histtype='step'
+    plt.hist([rank[s] for s in lang], 1000, histtype='step', cumulative=True, label=lang)
+    plt.legend()
+    f.close()
+
+draw("weekly-contest-326_lang_P1.txt")
+draw("weekly-contest-326_lang_P2.txt")
+draw("weekly-contest-326_lang_P3.txt")
+draw("weekly-contest-326_lang_P4.txt")
+plt.show()
