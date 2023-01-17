3b666e2e900bae2493b43d34cd6b999761cdf877
hqztrue <hqztrue@sina.com>
2023-01-16 22:31:21 -0600

    .

---
+++ LeetCode/LC_contest_lang/count_language.py
@@ -0,0 +1,22 @@
+from collections import Counter,defaultdict
+f=open("weekly-contest-327_lang_P1.txt")
+rank=defaultdict(list)
+a=[]
+for s in f.readlines():
+    l,r=s.split(',')[0],int(s.split(',')[1][1:])
+    a.append(l)
+    rank[l].append(r)
+
+c=Counter(a)
+print('total=',sum(c.values()))
+print(c)
+#print(rank)
+
+import matplotlib.pyplot as plt
+lang=['cpp','java','python3','golang']
+rank['python3']+=rank['python']
+#plt.hist([rank[s] for s in lang], 20, label=lang) #histtype='step'
+plt.hist([rank[s] for s in lang], 1000, histtype='step', cumulative=True, label=lang)
+plt.legend()
+plt.show()
+
